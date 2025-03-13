28c8f5ca39762c5019d25adf40224517f19a529d
TEJOCAHYO <tejo.cahyo.ft24@mail.umy.ac.id>
2025-03-13 11:19:25 +0700

    Membuat prosedur input

--- bubble.cpp
+++ bubble.cpp
@@ -2,52 +2,64 @@
 #include <string>
 using namespace std;
 
-int arr[20];    //Deklarasi variable global array dengan panjang 20 
-int n;  // Deklarasi variable global n untuk menyimpan elemen pada array
+int arr[20];    // Deklarasi variable global array dengan panjang 20
+int n;  // Deklarasi variable global n untuk menyimpan elemen pada array 
 
-void input (){  // prosedur input
+void input(){   // Prosedur input
     int d;
     while (true){
-        cout << "masukan banyaknya elemen pada aray : ";
+        cout << "Masukkan banyaknya elemen pada array: ";
         cin >> n;
         if (n <= 20){
             break;
         }
         else{
-            cout << "\nArray maksimal 20 element.\n";
+            cout << "\nArray maksimal 20 elemen.\n";
         }
     }
     cout << endl;
     cout << "=====================" << endl;
-    cout << "masukan element array" << endl;
+    cout << "Masukkan elemen array" << endl;
     cout << "=====================" << endl;
 
     for (int i=0;i<n;i++){
-        cout << "Data ke-" << (i+1) << " =  ";
+        cout << "Data ke-" << (i+1) << " = ";
         cin >> arr[i];
-
     }
 }
 
-void bubbleSortArray (){
-    int pass = 1;  // step 1
-    do{
+void bubbleSortArray(){
+    int pass = 1;   // step 1
+    do {
         for (int j=0;j<=n-1-pass;j++){ // step 2
-            if (arr[j] > arr [j+1]){   // step 3
+            if (arr[j] > arr[j+1]){     // step 3
                 int temp;
-                temp = arr [j];
+                temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
-             }
+            }
         }
         pass = pass + 1; // step 4
-     }while(pass <=n-1); // step 5
-   
-
+    }while(pass <=n-1);  // step 5
+       
 
 }
 
+void display(){
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    cout << endl;
+    for (int j=0;j<n;j++){
+        cout << arr[j];
+        if (j<n-1) {
+            cout << " --> ";
+        }
+    }
+}
 int main (){
-
-
+    input();
+    bubbleSortArray();
+    display();
 }
